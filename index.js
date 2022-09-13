3ae0039e38f3a995f10610a809350f07649e5a5a
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2022-09-13 08:05:54 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require ("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+
+dotenv.config();
+
+//connect to db
+mongoose.connect(
+    process.env.DB_CONNECT,
+    {useUnifiedTopology : true, useNewUrlparser: true},
+    () => console.log("connected to db")
+);
+
+app.listen(4000, () =>{console.log("server up and runing on port 4000!")} )
